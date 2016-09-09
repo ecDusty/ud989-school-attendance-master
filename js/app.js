64b390e4b87affd0f9eaa4eb3bc85ec4b587f0d3
Erin Mulligan <ecmgit@outlook.com>
2016-09-09 12:45:30 +0800

    refactoring the code
    First attempt at refactoring.

--- js/app.js
+++ js/app.js
@@ -82,3 +82,87 @@ $(function() {
 
     countMissing();
 }());
+
+
+model = {
+
+  students: [
+  /* Example of the Object structure within students Array
+    {
+      name: '',
+      attendance: []
+    }
+  */
+  ],
+
+  getInfo: function() {
+
+    if (!localStorage.attendance) {
+      console.log('Creating attendance records...');
+      this.createInfo();
+//
+//      function getRandom() {
+//        return (Math.random() >= 0.5);
+//      }
+
+      var student = $('tbody .name-col'),
+        attendance = {};
+
+      nameColumns.each(function() {
+        var name = this.innerText;
+        attendance[name] = [];
+
+        for (var i = 0; i <= 11; i++) {
+          attendance[name].push(getRandom());
+        }
+      });
+
+      localStorage.attendance = JSON.stringify(attendance);
+    }
+  },
+
+  createInfo: function(t) {
+
+
+
+  },
+
+  init: function() {
+    this.getInfo();
+  }
+
+
+};
+
+octopus = {
+  init: function () {
+    model.getInfo();
+    studentView.init();
+  }
+};
+
+studentView = {
+  getCurrentView: function() {
+    var currentView = [{name: '', attendance: []}]; // used to remind me of students data structure
+    var students = $('tbody .student');
+
+
+
+    nameColumns.each(function() {
+      var name = this.innerText;
+      var attendance = [];
+
+      for (var i = 0; i <= 11; i++) {
+        attendance.push(getRandom());
+      }
+    })
+  },
+
+  init: function() {
+
+  },
+
+  render: function() {
+
+  }
+}
